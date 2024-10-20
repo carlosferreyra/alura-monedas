a6f27ee7ca4ed95924177ef835f3240184b1d189
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2024-10-20 17:36:21 -0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sea bienvenido/a al conversor de moneda =]");
+    }
+    Vector<String> monedas = new Vector<>();
+//    continue su challenge aqui
+
+}
